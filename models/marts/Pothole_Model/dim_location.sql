b8ebb29f90b6dfae852c2216be817bbbe6d350c7
Zhe Wang <zhe.wang@baruchmail.cuny.edu>
2021-12-08 00:05:30 +0000

    second commit

--- models/marts/Pothole_Model/dim_location.sql
+++ models/marts/Pothole_Model/dim_location.sql
@@ -6,7 +6,8 @@
 
 with dim_location as 
 (SELECT DISTINCT
-    upper(Borough) as borough
- FROM `complaint-project-331901.311_Compaints.Weather_Raw`
+    upper(Borough) as borough,
+    incident_zip as zip
+ FROM `complaint-project-331901.311_Compaints.Pothole_Complaints`
 )
 select ROW_NUMBER () OVER() AS dim_location_id, * from dim_location
--- models/marts/Pothole_Model/fc_complaints.sql
+++ models/marts/Pothole_Model/fc_complaints.sql
@@ -4,26 +4,27 @@
   )
 }}
 
-with complaint as (
-  SELECT *
-  FROM {{ref ('stg_complaints')}}
-),
-
-complaint_date as(
-  select dim_date_id,substring(date_integer,3,6) as date_integer from {{ref ('dim_date')}}
-),
-
-complaint_loc as (
-  select * from {{ref ('dim_location')}}
-)
-
 select  
+    ROW_NUMBER () OVER() AS complaint_fact_id,
     dim_date_id,
     dim_location_id,
-    --Status_dim_ID,
-    --Agency_dim_ID,
+    dim_status_id,
+    dim_agency_id,
+    dim_channel_id,
+    --dim_complaint_type_id
     --Number_complain
-from complaint
-inner join complaint_date on complaint.create_date = complaint_date.date_integer
-inner join complaint_loc on complaint.borough = complaint_loc.borough
+from {{ref ('stg_complaint')}} as complaint
+inner join {{ref ('dim_date')}} as complaint_date on complaint.create_date = complaint_date.full_date
+inner join {{ref ('dim_location')}} as complaint_loc on complaint.borough = complaint_loc.borough
+inner join {{ref ('dim_status')}} as complaint_status on complaint.status = complaint_status.status
+inner join {{ref ('dim_agency')}} as complaint_agency on complaint.agency = complaint_agency.agency
+inner join {{ref ('dim_channel')}} as complaint_channel on complaint.channel = complaint_channel.channel
+
+ 
+
+
+
+
+
+
 
